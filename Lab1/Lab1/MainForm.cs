ca9929c697b73af62c471b12acab6af85f2420a7
Андрій Іл'ющенко <137763905+asiliushchenko@users.noreply.github.com>
2024-10-31 19:22:21 +0300

    Add files via upload

---
+++ Lab1/Lab1/MainForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+using static Lab1.OpenGL;
+
+
+namespace Lab1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lab1/Lab1/RenderControl/RenderControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class RenderControl : OpenGL
+    {
+        private Timer renderTimer;
+
+        public RenderControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RenderGL(object sender, EventArgs e)
+        {
+
+            glViewport(0, 0, Width, Height);
+            glLoadIdentity();
+            gluOrtho2D(-8, +12, -4, +8);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            Task task = new Task();
+            task.DrawGrid();     // Зображення координатної сітки
+            task.DrawFigure();    // Зображення фігури
+            task.DrawPoints();   // Зображення точок
+        }
+    }
+}
