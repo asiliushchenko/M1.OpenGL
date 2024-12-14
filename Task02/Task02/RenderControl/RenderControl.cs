4c15c71a52a988ea3113d80c00b714aea9cfc069
Андрій Іл'ющенко <137763905+asiliushchenko@users.noreply.github.com>
2024-12-14 14:24:10 +0200

    Add files via upload

---
+++ Task02/Task02/RenderControl/RenderControl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+using static Task02.RenderControl;
+
+namespace Task02
+{
+    public partial class RenderControl : OpenGL
+    {
+        public uint DrawMode { get; set; } = GL_FILL;
+        public int TilesVertical { get; set; } = 1;
+        public int TilesHorizontal { get; set; } = 1;
+        figure _f = new figure();
+
+        public RenderControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRender(object sender, EventArgs e)
+        {
+            int ortho = 10;
+
+            glClear(GL_COLOR_BUFFER_BIT);
+            glLoadIdentity();
+
+            if (Width > Height)
+                glViewport((Width - Height) / 2, 0, Height, Height);
+            else
+                glViewport(0, (Height - Width) / 2, Width, Width);
+
+            double max = Math.Max(TilesHorizontal, TilesVertical);
+            glOrtho(-50, 50 * max, -50, 50 * max, -1, 1);
+
+            double sideSize = 8.5;
+            double height = Math.Sqrt(3) / 2 * sideSize;
+
+            for (int row = 0; row < TilesVertical; row++)
+            {
+                for (int col = 0; col < TilesHorizontal; col++)
+                {
+                    double offsetX = col * 2.5 * sideSize;
+                    double offsetY = row * 2 * height;
+
+                    if (col % 2 != 0)
+                        offsetY -= height;
+
+                    _f.DrawComplexFigure(sideSize, DrawMode, offsetX, offsetY);
+                }
+            }
+
+            glDisable(GL_LINES);
+        }
+    }
+}
---
+++ Task02/Task02/figure.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task02
+{
+    public partial class RenderControl
+    {
+        public class figure 
+        {
+            readonly double cursorCenterX = -3;
+            readonly double cursorCenterY = 0;
+
+            public void DrawComplexFigure(double sidesize = 8.5, uint DrawMode = GL_FILL, double offsetX = 0, double offsetY = 0)
+            {
+                double height = Math.Sqrt(3) / 2 * sidesize; // Висота трикутника
+
+                glPolygonMode(GL_FRONT_AND_BACK, DrawMode);
+
+                // Лівий верхній жовтий чотирикутник (трапеція)
+                glBegin(GL_QUAD_STRIP);
+                glColor3d(1, 1, 0); // Жовтий колір
+                glVertex2d(offsetX, offsetY + height); // Лівий верх
+                glVertex2d(offsetX + sidesize, offsetY + height); // Правий верх
+                glVertex2d(offsetX - 4.25, offsetY); // Правий низ
+                glVertex2d(offsetX + sidesize + 4.25, offsetY); // Лівий низ
+                glEnd();
+
+                glBegin(GL_QUAD_STRIP);
+                glColor3d(1, 0, 0); // Червоний колір
+                glVertex2d(offsetX + sidesize, offsetY - height); // Лівий верх
+                glVertex2d(offsetX, offsetY - height); // Правий верх
+                glVertex2d(offsetX + sidesize + 4.25, offsetY); // Правий низ
+                glVertex2d(offsetX - 4.25, offsetY); // Лівий низ
+                glEnd();
+
+                glBegin(GL_TRIANGLES);
+                glColor3d(1, 1, 1); // Білий колір
+                glVertex2d(offsetX + sidesize, offsetY + height); // Лівий кут
+                glVertex2d(offsetX + sidesize * 2 , offsetY + height); // Правий кут
+                glVertex2d(offsetX + sidesize + 4.25, offsetY); // Нижній кут
+                glEnd();
+
+                glBegin(GL_TRIANGLES);
+                glColor3d(0.5, 0.5, 0.5); // Сірий колір
+                glVertex2d(offsetX + sidesize + 4.25, offsetY); // Верхній кут
+                glVertex2d(offsetX + sidesize, offsetY - height); // Лівий кут
+                glVertex2d(offsetX + sidesize * 2, offsetY - height); // Правий кут
+                glEnd();
+
+                glBegin(GL_TRIANGLES);
+                glColor3d(1, 0, 0);
+                glVertex2d(offsetX + sidesize * 2, offsetY + height); // Правий кут
+                glVertex2d(offsetX + sidesize + 4.25, offsetY); // Лівий кут
+                glVertex2d(offsetX + sidesize * 2 + 4.25, offsetY); // Правий кут
+                glEnd();
+
+                glBegin(GL_TRIANGLES);
+                glColor3d(1, 1, 0);
+                glVertex2d(offsetX + sidesize + 4.25, offsetY); // Лівий кут
+                glVertex2d(offsetX + sidesize * 2 + 4.25, offsetY); // Правий кут
+                glVertex2d(offsetX + sidesize * 2, offsetY - height); // Нижній кут
+                glEnd();
+            }
+        }
+    }
+}
